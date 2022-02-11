b8a1ac86ab01e5a207d929d7ab3b918811880aae
guochao <463561248@qq.com>
2022-02-11 17:10:18 +0800

    ✨ feat:  添加prettier

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+/*
+ * @Author: saber
+ * @Date: 2022-02-10 15:41:57
+ * @LastEditTime: 2022-02-11 17:04:07
+ * @LastEditors: saber
+ * @Description:
+ */
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,44 @@
 /*
  * @Author: saber
  * @Date: 2022-02-10 15:41:57
- * @LastEditTime: 2022-02-10 18:52:13
+ * @LastEditTime: 2022-02-11 17:01:45
  * @LastEditors: saber
  * @Description:
  */
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
-import Icons from 'unplugin-icons/vite'
-import { FileSystemIconLoader } from 'unplugin-icons/loaders'
-import IconsResolver from 'unplugin-icons/resolver'
-import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), Icons({
-    compiler: 'vue3',
-    customCollections: {
-      custom: FileSystemIconLoader('src/assets/icons1')
-    }
-  }),
-  Components({
-    dts: true,
-    resolvers: [
-      IconsResolver({
-        customCollections: ['custom'],
-      }),
-    ],
-  }), 
-],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Icons({
+      compiler: 'vue3',
+      customCollections: {
+        custom: FileSystemIconLoader('src/assets/icons1'),
+      },
+    }),
+    Components({
+      dts: true,
+      resolvers: [
+        IconsResolver({
+          customCollections: ['custom'],
+        }),
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
