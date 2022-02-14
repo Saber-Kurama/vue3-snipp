cbe809a4c997913e1e6cba1bd63c6eff6419c471
guochao <463561248@qq.com>
2022-02-14 10:13:19 +0800

    ✨ feat:  调整eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,25 @@
+/*
+ * @Author: saber
+ * @Date: 2022-02-10 15:41:57
+ * @LastEditTime: 2022-02-11 19:04:12
+ * @LastEditors: saber
+ * @Description:
+ */
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
-}
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+};
